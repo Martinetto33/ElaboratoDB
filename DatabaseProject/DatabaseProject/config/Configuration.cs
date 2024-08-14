0dae34c495952291272e91225c8c3b39e7163ff4
Alin Bordeianu <alinstefan.bordeianu@studio.unibo.it>
2024-08-14 17:04:28 +0200

    docs: updated readme with instructions on how to get correct nuget packages; created connection string in Configuration.cs

--- DatabaseProject/DatabaseProject/config/Configuration.cs
+++ DatabaseProject/DatabaseProject/config/Configuration.cs
@@ -11,5 +11,9 @@ namespace DatabaseProject.config
         public static readonly double UPGRADE_TIME_PER_LEVEL = 5.0; // the number of seconds per level to upgrade a building or troop
 
         public static readonly int MAX_LEVEL = 6; // the maximum level for a building or troop
+
+        public static readonly string DB_NAME = "clash_of_clans"; // the name of the database
+
+        public static readonly string CONNECTION_STRING = $"server=localhost;port=3306;database={DB_NAME};user=root";
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ElaboratoDB
+
+## Packages installation with NuGet
+[Official guide](https://www.nuget.org/packages/EntityFramework/)
+Open your NuGet terminal in Visual Studio by selecting Tools > NuGet Package Manager > Package Manager Console.
+Type in the following command:
+```
+NuGet\Install-Package EntityFramework -Version 6.5.1
+```
+
+And for MySQL Data:
+```
+NuGet\Install-Package MySql.Data -Version 9.0.0
+```
